93211e08a1bb2ff0c2a08a6f40e905b98441231b
henry <hyliang@gmail.com>
2025-12-25 23:20:25 +0800

    update nginx-with-crowdsec

--- nginx-with-crowdsec/entrypoint.sh
+++ nginx-with-crowdsec/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# vim:sw=4:ts=4:et
 
 set -e
 
@@ -9,14 +8,20 @@ entrypoint_log() {
     fi
 }
 
-ME=$(basename "$0")
+if [ "$1" = "nginx" ]; then
 
-CONFIG_PATH="/etc/crowdsec/bouncers/"
+    ME=$(basename "$0")
 
-if [ -n "$API_KEY" -a -n "$CROWDSEC_LAPI_URL" ];then
-    API_KEY=${API_KEY} CROWDSEC_LAPI_URL=${CROWDSEC_LAPI_URL} envsubst '$API_KEY $CROWDSEC_LAPI_URL' < ${CONFIG_PATH}crowdsec-nginx-bouncer.conf.example | tee -a "${CONFIG_PATH}crowdsec-nginx-bouncer.conf" >/dev/null
-    entrypoint_log "$ME: info: Use CROWDSEC_LAPI_URL $CROWDSEC_LAPI_URL"
+    CONFIG_PATH="/etc/crowdsec/bouncers/"
+
+    if [ -n "$API_KEY" -a -n "$CROWDSEC_LAPI_URL" ];then
+        API_KEY=${API_KEY} CROWDSEC_LAPI_URL=${CROWDSEC_LAPI_URL} envsubst '$API_KEY $CROWDSEC_LAPI_URL' < ${CONFIG_PATH}crowdsec-nginx-bouncer.conf.example | tee -a "${CONFIG_PATH}crowdsec-nginx-bouncer.conf" >/dev/null
+        entrypoint_log "$ME: info: Use CROWDSEC_LAPI_URL $CROWDSEC_LAPI_URL"
+    fi
+
 fi
+
+
 
 
 exec "$@"
